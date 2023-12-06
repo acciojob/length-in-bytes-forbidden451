037af756a4fc8856ae4aeb3be04f7f6b2c000eec
Deepak Kanshetti <kanshettid@gmail.com>
2023-12-06 21:45:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	const encoder = new TextEncoder('utf-8');
+
+  // Encode the string into a Uint8Array
+  const encodedArray = encoder.encode(str);
+
+  // Return the byte size (length of the Uint8Array)
+  return encodedArray.length;  
 };
 
 // Do not change the code below
